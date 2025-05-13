8714df954eaa536a030175ff0745d64eaf4e36b9
Jacobo Vidal <jacobovidal@users.noreply.github.com>
2025-05-13 16:33:55 +0200

    Update code snippets

--- examples/example-react/src/utils/code-snippets.ts
+++ examples/example-react/src/utils/code-snippets.ts
@@ -10,16 +10,20 @@ await publicClient.get("/post/1");`;
 export const bearerClientSnippet = `import { OAuthFetch } from "oauth-fetch";
 import { useAuth0 } from "@auth0/auth0-react";
 
+import { Auth0TokenProvider } from "@/utils/auth0-token-provider";
+
 const auth0 = useAuth0();
 
 const bearerClient = new OAuthFetch({
   baseUrl: "https://auth0.oauthlabs.com",
-  tokenProvider: new Auth0TokenProvider(useAuth0()),
+  tokenProvider: new Auth0TokenProvider(auth0),
 });
 
 await bearerClient.get("/userinfo");`;
 
 export const dpopClientSnippet = `import { OAuthFetch, DPoPUtils } from "oauth-fetch";
+
+import { DuendeTokenProvider } from "@/utils/duende-token-provider";
 
 const dpopKeyPair = await DPoPUtils.generateKeyPair({
   algorithm: "ECDSA",
